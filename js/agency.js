18674fdd37e7224ae453bd4f8c0e778c2a7a1e4a
Paulo Pinho <paulo.pinho@gotvmediasoftware.com>
2020-04-18 17:16:03 +0100

    add new logos create new html page for each item in Portfolio

--- js/agency.js
+++ js/agency.js
@@ -26,32 +26,5 @@
     offset: 56
   });
 
-  $('.showhide').click(function() {
-      //$("#modal").removeClass('modal-dialog');
-      if ($("#modal").hasClass('show-simulation')){
-        $("#modal").removeClass('show-simulation');
-      } else {
-         $("#modal").addClass('show-simulation');
-      }
-        var x = document.getElementById("simulationDialog");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
+  
 })(jQuery); // End of use strict
---
+++ js/mslweb.js
@@ -0,0 +1,41 @@
+$(".portfolio-item").click(function(){
+    console.log($(this).data("item"));
+    $(".modal-content-load").load("/Portfolio/"+$(this).data("item")+".html", function(){
+
+        $('.showhide').click(function() {   
+            //$("#modal").removeClass('modal-dialog');
+            if ($("#modal").hasClass('show-simulation')){
+              $("#modal").removeClass('show-simulation');
+            } else {
+               $("#modal").addClass('show-simulation');
+            }
+    
+            var x = document.getElementById("simulationDialog");
+            if (x.style.display === "none") {
+            x.style.display = "block";
+            } else {
+            x.style.display = "none";
+            }
+        });
+
+    });
+});
+
+
+
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
+
+
+
+
